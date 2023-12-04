1f2cb64d84e544290c6ef50030c95a42ca747315
Andrew Parker <andrew@avparker.com>
2023-12-04 23:10:38 +0900

    Implement day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun lineCalibration(line: String): Int {
+        val digits = line.filter { it.isDigit() }
+        return "${digits.first()}${digits.last()}".toInt()
     }
 
+    fun part1(input: List<String>): Int {
+        return input.sumOf {  lineCalibration(it) }
+    }
+
+    fun lineCalibration2(line: String): Int {
+        val replaced = line.mapIndexed{ i, c ->
+            if (c.isDigit()) c
+            else {
+                val remaining = line.substring(i, (i+5).coerceAtMost(line.length))
+                if (remaining.startsWith("one") ) "1"
+                else if (remaining.startsWith("two") ) "2"
+                else if (remaining.startsWith("three") ) "3"
+                else if (remaining.startsWith("four") ) "4"
+                else if (remaining.startsWith("five") ) "5"
+                else if (remaining.startsWith("six") ) "6"
+                else if (remaining.startsWith("seven") ) "7"
+                else if (remaining.startsWith("eight") ) "8"
+                else if (remaining.startsWith("nine") ) "9"
+                else ""
+            }
+        }.toString()
+        return lineCalibration(replaced)
+    }
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {  lineCalibration2(it) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(lineCalibration("1abc2") == 12)
+    check(lineCalibration("pqr3stu8vwx") == 38)
+    check(lineCalibration("a1b2c3d4e5f") == 15)
+    check(lineCalibration("treb7uchet") == 77)
+    check(part1(testInput) == 142)
+
+    val testInput2 = readInput("Day01_test2")
+    check(part2(listOf("two1nine")) == 29)
+    check(part2(listOf("eightwothree")) == 83)
+    check(part2(listOf("abcone2threexyz")) == 13)
+    check(part2(listOf("xtwone3four")) == 24)
+    check(part2(listOf("4nineeightseven2")) == 42)
+    check(part2(listOf("zoneight234")) == 14)
+    check(part2(listOf("7pqrstsixteen")) == 76)
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
